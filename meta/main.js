f690145cf4d88c77ff5dc923bf05ee1d60a3b28b
hyukychang <seunghyuk.dev@gmail.com>
2025-05-30 11:10:31 -0700

    step1.2

--- meta/main.js
+++ meta/main.js
@@ -164,6 +164,18 @@ function renderScatterPlot(data, commits) {
     .axisLeft(yScale)
     .tickFormat((d) => String(d % 24).padStart(2, "0") + ":00");
 
+  svg
+    .append("g")
+    .attr("transform", `translate(0, ${usableArea.bottom})`)
+    .attr("class", "x-axis") // new line to mark the g tag
+    .call(xAxis);
+
+  svg
+    .append("g")
+    .attr("transform", `translate(${usableArea.left}, 0)`)
+    .attr("class", "y-axis") // just for consistency
+    .call(yAxis);
+
   // Add gridlines BEFORE the axes
   const gridlines = svg
     .append("g")
@@ -303,6 +315,57 @@ function renderLanguageBreakdown(selection) {
   }
 }
 
+function updateScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3.select("#chart").select("svg");
+
+  xScale = xScale.domain(d3.extent(commits, (d) => d.datetime));
+
+  const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+  const rScale = d3.scaleSqrt().domain([minLines, maxLines]).range([2, 30]);
+
+  const xAxis = d3.axisBottom(xScale);
+
+  // CHANGE: we should clear out the existing xAxis and then create a new one.
+  const xAxisGroup = svg.select("g.x-axis");
+  xAxisGroup.selectAll("*").remove();
+  xAxisGroup.call(xAxis);
+
+  const dots = svg.select("g.dots");
+
+  const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+  dots
+    .selectAll("circle")
+    .data(sortedCommits)
+    .join("circle")
+    .attr("cx", (d) => xScale(d.datetime))
+    .attr("cy", (d) => yScale(d.hourFrac))
+    .attr("r", (d) => rScale(d.totalLines))
+    .attr("fill", "steelblue")
+    .style("fill-opacity", 0.7) // Add transparency for overlapping dots
+    .on("mouseenter", (event, commit) => {
+      d3.select(event.currentTarget).style("fill-opacity", 1); // Full opacity on hover
+      renderTooltipContent(commit);
+      updateTooltipVisibility(true);
+      updateTooltipPosition(event);
+    })
+    .on("mouseleave", (event) => {
+      d3.select(event.currentTarget).style("fill-opacity", 0.7);
+      updateTooltipVisibility(false);
+    });
+}
+
 let data = await loadData();
 let commits = processCommits(data);
 
@@ -325,6 +388,8 @@ let commitMaxTime = timeScale.invert(commitProgress);
 const slider = document.getElementById("commit-progress");
 const timeEl = document.getElementById("commit-time");
 
+let filteredCommits = commits;
+
 function onTimeSliderChange() {
   commitProgress = +slider.value;
 
@@ -334,6 +399,9 @@ function onTimeSliderChange() {
     dateStyle: "long",
     timeStyle: "short",
   });
+
+  filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
+  updateScatterPlot(data, filteredCommits);
 }
 
 slider.addEventListener("input", onTimeSliderChange);

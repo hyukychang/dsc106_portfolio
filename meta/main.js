d7147e8b22bfc625c7ec0d4faae166dd3bf9087b
hyukychang <seunghyuk.dev@gmail.com>
2025-05-30 21:04:20 -0700

    lab finish

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,5 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+import scrollama from "https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm";
 
 let xScale;
 let yScale;
@@ -426,9 +427,12 @@ function onTimeSliderChange() {
   updateFileDisplay(filteredCommits);
 }
 
-slider.addEventListener("input", onTimeSliderChange);
+// slider.addEventListener("input", onTimeSliderChange);
 
-onTimeSliderChange();
+// onTimeSliderChange();
+
+updateScatterPlot(data, filteredCommits);
+updateFileDisplay(filteredCommits);
 
 function updateFileDisplay(filteredCommits) {
   lines = filteredCommits.flatMap((d) => d.lines);
@@ -470,3 +474,48 @@ function updateFileDisplay(filteredCommits) {
 }
 
 updateFileDisplay(filteredCommits);
+
+d3.select("#scatter-story")
+  .selectAll(".step")
+  .data(commits)
+  .join("div")
+  .attr("class", "step")
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString("en", {
+      dateStyle: "full",
+      timeStyle: "short",
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? "another glorious commit" : "my first commit, and it was glorious"
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very good.
+	`
+  );
+
+// function onStepEnter(response) {
+//   console.log(response);
+// }
+function onStepEnter(response) {
+  const lastCommit = response.element.__data__;
+  const lastCommitTime = lastCommit.datetime;
+  // console.log(response.element.__data__.datetime);
+  filteredCommits = commits.filter((d) => d.datetime <= lastCommitTime);
+  updateScatterPlot(data, filteredCommits);
+  updateFileDisplay(filteredCommits);
+}
+
+const scroller = scrollama();
+scroller
+  .setup({
+    container: "#scrolly-1",
+    step: "#scrolly-1 .step",
+  })
+  .onStepEnter(onStepEnter);
